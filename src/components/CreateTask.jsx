a9e946a348c38a900b14135b3f188c94e4fe6085
kema-ray <rachelkemuma99@gmail.com>
2023-10-03 12:10:17 +0300

    create a state for a single task

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
 const CreateTask = ({ tasks, setTasks }) => {
+    
+    const [task, setTask] = useState({
+        id:"",
+        name:"",
+        status:"todo"  // can also be inprogress, backlog or complete
+    });
+
+    console.log(task);
+
     return (
     <form>
         <input type="text" className="border-2 border-slate-400 bg-slate100 rounded-md 
-        mr-4 h-12 w-64 px-1" />
+        mr-4 h-12 w-64 px-1"
+        value={task.name}
+        onChange={(e) =>
+            setTask({...task, id: uuidv4(), name: e.target.value})
+        }
+        />
         <button className="bg-cyan-500 rounded-md px-4 h-12 text-white">Create</button>
     </form>
     );
