6481a6cb8736e9df443e840e3736926f9b46d762
kema-ray <rachelkemuma99@gmail.com>
2023-10-03 12:34:48 +0300

    useEffect hook that allows the tasks to remain on state on refresh of the browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateTask from "./components/CreateTask";
 import ListTasks from "./components/ListTasks";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  
+
   console.log("tasks", tasks)
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("tasks")));
+  }, [])
 
   return (
     <div className="bg-slate-100 w-screen h-screen flex flex-col items-center pt-3 gap-16">
