e7d229f9a3ee574733d035585c08832b59f9b83a
kema-ray <rachelkemuma99@gmail.com>
2023-10-04 09:51:31 +0300

    create reusable components

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -20,17 +20,39 @@ const ListTasks = ({ tasks, setTasks }) => {
 
     return (
         <div className="flex gap-16">
-            {statuses.map((status, index) => <Section key={index} status = {status} />)}
+            {statuses.map((status, index) => (
+                <Section 
+                    key={index} 
+                    status = {status} 
+                    tasks={tasks} 
+                    setTasks={setTasks} 
+                    todos={todos} 
+                    inProgress={inProgress} 
+                    completed={completed} 
+                />
+            ))}
         </div>
     );
 }
 
 export default ListTasks;
 
-const Section = ({status}) => {
+const Section = ({status, tasks, setTasks, todos, inProgress, completed}) => {
+    let text = "Todo"
+    let bg = "bg-slate-500"
+
     return (
-        <div>
-            <h2>{status}</h2> List
+        <div className={`w-64`}>
+           <Header text={text} bg={bg} count={todos.length} /> List
+        </div>
+    )
+}
+
+const Header = ({ text, bg, count}) => {
+    return (
+        <div className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white`}>
+           {text}
+           <div className="ml-2 bg-white h-5 w-5 text-black rounded-full flex items-center justify-center">{count}</div> 
         </div>
     )
 }
