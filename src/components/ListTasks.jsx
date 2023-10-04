6d107dcf31c2a653a46228acc390aecc65750531
kema-ray <rachelkemuma99@gmail.com>
2023-10-04 09:13:34 +0300

    add useState and useEffect hooks for the list tasks

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 const ListTasks = ({ tasks, setTasks }) => {
+
+    const [todos, setTodos] = useState([]);
+    const [inProgress, setInProgress] = useState([]);
+    const [completed, setCompleted] = useState([]);
+
+    useEffect(() => {
+        const filterTodos = tasks.filter(task => task.status === "todo");
+        const filterInProgress = tasks.filter(task => task.status === "inprogress");
+        const filterCompleted = tasks.filter(task => task.status === "completed");
+
+        setTodos(filterTodos);
+        setInProgress(filterInProgress);
+        setCompleted(filterCompleted);
+    }, [tasks])
+
     return <div>List</div>;
 }
 
