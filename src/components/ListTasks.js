46642ef11e9af38421963de13c3701501b7e7241
kema-ray <rachelkemuma99@gmail.com>
2023-10-12 16:18:04 +0300

    make changes to the list task component

--- src/components/ListTasks.js
+++ src/components/ListTasks.js
@@ -9,11 +9,11 @@ const ListTasks = ({ tasks, setTasks }) => {
     const [completed, setCompleted] = useState([]);
 
     useEffect(() => {
-        const fTodos = tasks.filter(task => task.status === "todo");
+        const filterTodos = tasks.filter(task => task.status === "todo");
         const filterInProgress = tasks.filter(task => task.status === "inprogress");
         const filterCompleted = tasks.filter(task => task.status === "completed");
 
-        setTodos(fTodos);
+        setTodos(filterTodos);
         setInProgress(filterInProgress);
         setCompleted(filterCompleted);
     }, [tasks]);
